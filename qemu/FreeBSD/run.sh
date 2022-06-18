8cfa63500168d4fac2c0eba9156cca8ca5dd7fa1
Andrew <rtpdfv@yahoo.com>
2022-06-18 18:57:29 +0200

    .

---
+++ qemu/FreeBSD/run.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# qemu-img create -f qcow2 virtual_*.img 30G
+# kvm -hda virtual_*.img -cdrom ./*.iso -m 2048 -net nic -net user -soundhw all
+
+kvm -hda \
+	$(ls ./virtual_*.img) \
+	-m 2048 \
+	-net nic \
+	-net user \
+	-soundhw all \
