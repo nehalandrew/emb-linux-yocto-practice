bf2650f957b70339c72b5061e3e5acd962362067
Andrew <rtpdfv@yahoo.com>
2022-06-19 02:36:49 +0200

    just commit

--- qemu/template_box/run.sh
+++ qemu/template_box/run.sh
@@ -3,9 +3,9 @@
 # qemu-img create -f qcow2 virtual_*.img 30G
 # kvm -hda virtual_*.img -cdrom ./*.iso -m 2048 -net nic -net user -soundhw all
 
-kvm -hda \
-	$(ls ./virtual_*.img) \
+kvm \
+	-hda $(ls ./virtual_*.img) \
 	-m 2048 \
 	-net nic \
 	-net user \
-	-soundhw all \
+	-soundhw all;
