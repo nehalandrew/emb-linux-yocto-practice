e9eb84359ac58ea8b66e162411860b955d45a988
Andrew <rtpdfv@yahoo.com>
2022-07-16 18:48:10 +0200

    just lazy commit

--- qemu/template_box/run.sh
+++ qemu/template_box/run.sh
@@ -3,9 +3,14 @@
 # qemu-img create -f qcow2 virtual_*.img 30G
 # kvm -hda virtual_*.img -cdrom ./*.iso -m 2048 -net nic -net user -soundhw all
 
-kvm \
+/usr/bin/qemu-system-x86_64 \
 	-hda $(ls ./virtual_*.img) \
 	-m 2048 \
+	-soundhw all \
+	-name vserialtest \
+	-chardev socket,path=/tmp/port1,server,nowait,id=port1-char \
+	-device virtio-serial \
+	-device virtserialport,id=port1,chardev=port1-char,name=org.fedoraproject.port.0 \
 	-net nic \
-	-net user \
-	-soundhw all;
+	-net user,hostfwd=tcp::10022-:22,hostfwd=tcp::10080-:80 \
+	;
