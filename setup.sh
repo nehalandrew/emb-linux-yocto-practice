50cfcf5742b813143f89cedf0c29119445174809
Andrii Nehalchuk <77306153+andneh@users.noreply.github.com>
2023-05-13 23:32:53 +0000

    .

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+# setup
+sudo apt update
+sudo apt install -y gawk wget git diffstat unzip texinfo gcc build-essential chrpath socat cpio python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev python3-subunit mesa-common-dev zstd liblz4-tool file locales
+sudo apt full-upgrade -y
+sudo locale-gen en_US.UTF-8
+
+git submodule update --init --recursive
+
+# env
+cd sources/poky/
+source oe-init-build-env
+
+
+# build
+bitbake core-image-sato
+
+# test
+runqemu qemux86-64
